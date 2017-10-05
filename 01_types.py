606fb1b942ab73abd21be8cdb37b4eff7871ce74
usr <usr@MacBook-Air-adm.local>
2017-10-05 20:40:25 +0200

    file layout change

---
+++ 01_types.py
@@ -0,0 +1,12 @@
+
+a = 'hello' # string
+b = 10      # int
+c = 0.5     # float
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
--- conditional.py
+++
@@ -1,6 +0,0 @@
-balance = 10
-print("current balance {}".format(balance))
-
-if balance < 10:
-  balance += 10
-  print("current balance {}".format(balance))
