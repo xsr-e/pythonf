31a827ee96d8a0370589730e244ab828308fe077
usr <12829148+xsr-e@users.noreply.github.com>
2017-10-12 22:59:15 +0200

    data stuctures

---
+++ 12_list_set_dictionary.py
@@ -0,0 +1,14 @@
+list_int = [1,2,2,2,3]
+print (list_int)
+
+set_int = set(list_int) #order is not guaranteed
+print (set_int)
+
+dict_int = {1:'A',2:'B'}
+print(dict_int)
+print(dict_int[2])
+
+n=dict_int.get(100)
+print(n)
+
+dict_int[100]
