71f1f32408b41f24729ed8ed1031ec7508af2200
usr <12829148+xsr-e@users.noreply.github.com>
2017-10-13 16:42:45 +0200

    data stuctures

--- 12_list_set_dictionary.py
+++ 12_list_set_dictionary.py
@@ -1,9 +1,14 @@
+
+# list
 list_int = [1,2,2,2,3]
 print (list_int)
 
+
+#set
 set_int = set(list_int) #order is not guaranteed
 print (set_int)
 
+#dictionary
 dict_int = {1:'A',2:'B'}
 print(dict_int)
 print(dict_int[2])
@@ -11,4 +16,4 @@ print(dict_int[2])
 n=dict_int.get(100)
 print(n)
 
-dict_int[100]
+# dict_int[100] # KeyError 100
