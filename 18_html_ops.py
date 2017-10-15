cf3169189cff2b05f7e8328cc6365e8344d25843
usr <12829148+xsr-e@users.noreply.github.com>
2017-10-15 23:26:22 +0200

    data stuctures

--- 18_html_ops.py
+++ 18_html_ops.py
@@ -1,6 +1,8 @@
 import requests
 import bs4
-url = 'https://www.microsoft.com/en-us/learning/course.aspx?cid=20483'
+course_number=20486
+url = "https://www.microsoft.com/en-us/learning/course.aspx?cid={}".format(course_number)
+print url
 r =requests.get(url)
 print ((r.status_code, r.encoding))
 
@@ -9,11 +11,15 @@ if(r.status_code == 200):
     course_title = s.title.string
     print(course_title)
     c_info= s.find('dd',{'id':'course-info'})
-    print c_info
+    #print c_info
     c_overwiev =s.find('dd',{'id':'overview'})
-    print  c_overwiev.text
+    #print  c_overwiev.text
     c_syllabus =s.find('dd',{'id':'syllabus'})
-    print  c_syllabus
+    #print  c_syllabus
     mods = s.find_all('span',{'class':'DetailPagesContentLeadIn'})
     for m in mods:
-        print m
+        if m.text[:6] == 'Module':
+            print m.text
+        else:
+            print "\t"+m.text
+    print "\n course {}, consist of {} modules".format(course_title, len(mods)/2)
